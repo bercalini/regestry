a0f4e3e21e78f1029ce2354d0dc2dbce10fc053c
bercalinidev <alisson.develooper@hotmail.com]>
2022-04-27 13:44:27 -0300

    service-regestry

---
+++ service-resistry/src/test/java/com/bercalini/serviceresistry/ServiceResistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bercalini.serviceresistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceResistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
